593bec40705c3e8e67b91144eb910e11f7d3c440
Claudie Gueguen <gueguen_c@yahoo.fr>
2025-08-19 06:08:26 +0200

    create global trend

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 df = pd.read_csv("data/owid-co2-data.csv")
 
-print(df.head())
-print(df.columns)
+cols = ["country", "year", "co2", "co2_per_capita", "population"]
+df_small = df[cols].copy()
+
+print("Aperçu du DataFrame simplifié :")
+print(df_small.head(), "\n")
+
+global_trend = (
+    df_small.groupby("year")["co2"]
+    .sum()
+    .reset_index(name="global_co2")
+)
+
+print("Évolution globale des émissions (quelques lignes) :")
+print(global_trend.head(20))
